9c976721b7ffe029af5a772a58cbfde813f6bb30
krishna M <krishnamurugan589@gmail.com>
2020-05-11 12:42:34 +0530

    First Commit

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="movie-card">
+      <img 
+        :src="movie.movieImage"
+        class="movie-image"
+      />
+    </div>
+</template>
+
+<style scoped>
+
+.movie-card {
+    height: 200px;
+    width: 200px;
+}
+
+.movie-card:hover{
+    cursor: pointer;
+}
+
+.movie-image {
+    height: 100%;
+    width: 100%;
+}
+
+</style>
+
+<script>
+export default {
+    name: "MovieCard",
+    props: {
+        movie: {
+            type: Object
+        }
+    }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <MovieCard v-for="movie in result" :key="movie._id" :movie="movie" />
   </div>
 </template>
 
+<style scoped>
+.home {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-auto-rows: 200px;
+  row-gap: 1em;
+}
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
+
+import config from "../services/config"
+import MovieCard from "../components/MovieCard"
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    MovieCard
+  },
+  data() {
+    return {
+      result: []
+    };
+  },
+  async beforeCreate() {
+    await axios.get(`${config.BASE_URL}/movies`)
+      .then(response => {
+        this.result = response.data.data
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
 };
 </script>
